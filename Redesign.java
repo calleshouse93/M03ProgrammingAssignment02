c346cee3614d0fe4a627829dd51ee8f100510fb1
calleshouse93 <149311867+calleshouse93@users.noreply.github.com>
2023-11-12 23:01:21 -0500

    Add files via upload

---
+++ Redesign.java
@@ -0,0 +1,22 @@
+
+import java.math.BigInteger;
+
+
+public class Redesign {
+
+	public static void main(String[] args) {
+		Rational r1 = new Rational(new BigInteger("400000"),
+				new BigInteger("200000"));
+		Rational r2 = new Rational(new BigInteger("200000"),
+				new BigInteger("300000"));
+		
+		System.out.println(r1 + " + " + r2 + " = " + r1.add(r2));
+		System.out.println(r1 + " - " + r2 + " = " + r1.subtract(r2));
+		System.out.println(r1 + " * " + r2 + " = " + r1.multiply(r2));
+		System.out.println(r1 + " / " + r2 + " = " + r1.divide(r2));
+		System.out.println(r2 + " is " + r2.doubleValue());
+		
+
+	}
+
+}
